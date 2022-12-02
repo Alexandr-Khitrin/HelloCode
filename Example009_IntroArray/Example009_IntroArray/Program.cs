3c521db2cf2dc46410a4c60e38479273c394c430
Alexandr <hitrin79@gmail.com>
2022-12-02 18:47:01 +0800

    Знакомство с функциями и массивами

---
+++ Example009_IntroArray/Example009_IntroArray/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {1, 2, 3, 4, 51, 6, 7, 8, 9};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
+
+//int max = a1;
+//if (b1 > max) max = b1;
+//if (c1 > max) max = c1;
+//if (a2 > max) max = a2;
+//if (b2 > max) max = b2;
+//if (c2 > max) max = c2;
+//if (a3 > max) max = a3;
+//if (b3 > max) max = b3;
+//if (c3 > max) max = c3;
+
+
+
---
+++ Example010_MethodArray/Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {122, 2543, 122, 45456, 5332, 5346, 7534, 856435};
+
+int n = array.Length;
+
+int find = 122;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
